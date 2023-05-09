ec667d6fa8a34d7a55affe7b87bd6a09f23a7a69
emrushmaliqi <emrushmaliqi2014@gmail.com>
2023-05-09 12:47:03 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import WatchList from "./pages/WatchList";
+import Wallet from "./pages/Wallet";
+import Token from "./pages/Token";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classNameName="App">
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/watchlist" element={<WatchList />} />
+        <Route exact path="/wallet" element={<Wallet />} />
+        <Route exact path="/token/:id" element={<Token />} />
+      </Routes>
     </div>
   );
 }
